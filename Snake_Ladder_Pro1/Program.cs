1f8fe5eced7d43ce92ca37c1e55986e38c8ad69e
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-02 20:19:25 +0530

    [Parnika]:The player then checks for an option.They are NO Play,Ladder or Snake

--- Snake_Ladder_Pro1/Program.cs
+++ Snake_Ladder_Pro1/Program.cs
@@ -4,13 +4,38 @@
 
 internal class Programm
 {
+    const int NO_PLAY= 1;
+    const int LADDER= 2;
+    const int SNAKE= 3;
+
     public static void Main(String[] args) 
     {
         int position = 0;
         
+        //simulate rolling a die (1 to 6)
         Random random = new Random();   
         int diceValue=random.Next(1,7);
         Console.WriteLine("welcome to Snake Ladder Game...lets start");
-        Console.WriteLine("You have got dicevalue: "+diceValue);
+        //simulate option: NO_PLAY,LADDER,SNAKE
+        Random random1 = new Random();
+        int option=random1.Next(1,4);
+        switch(option) 
+        {
+            case NO_PLAY:
+                Console.WriteLine("no play");
+                break; 
+            case LADDER:
+                Console.WriteLine("ladder");
+                position +=diceValue ;
+                Console.WriteLine("You are at position: "+position);
+                break; 
+            case SNAKE:
+                Console.WriteLine("Snake");
+                position -=diceValue ;
+                Console.WriteLine("You are at position: " + position);
+                break;
+
+
+        }
     }
 }
