3952577df011c50053750d99708084c79b2fed72
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-02 19:47:08 +0530

    [parnika]:Starting Snake Ladder game from position 0

---
+++ Snake_Ladder_Pro1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+internal class Programm
+{
+    public static void Main(String[] args) 
+    {
+        int position = 0;
+        Console.WriteLine("welcome to Snake Ladder Game...lets start");
+    }
+}
