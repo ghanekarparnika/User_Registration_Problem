a4eb4a9845280849eb26856e65d01c7ab76f8eb8
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-02 20:01:35 +0530

    [parnika]:the player rolls the die to get a number between 1 to 6

--- Snake_Ladder_Pro1/Program.cs
+++ Snake_Ladder_Pro1/Program.cs
@@ -1,11 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+//UC2-The player rolls the die to get a number between 1 to 6
 
 internal class Programm
 {
     public static void Main(String[] args) 
     {
         int position = 0;
+        
+        Random random = new Random();   
+        int diceValue=random.Next(1,7);
         Console.WriteLine("welcome to Snake Ladder Game...lets start");
+        Console.WriteLine("You have got dicevalue: "+diceValue);
     }
 }
