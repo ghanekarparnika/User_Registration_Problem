4d19544816fe9538008fb4ab6eb905b84c8ad343
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-05 13:50:35 +0530

    [parnika]:understood Inheritance concept

---
+++ Oops_Concept_Assignment/Inheritance.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Concept_Assignment
+{
+    internal class Inheritance
+    {
+
+    }
+
+    class Vehicle 
+    {
+        public void start()
+        {
+            Console.WriteLine("Start engine..");
+        }
+
+        public void stop() 
+        {
+            Console.WriteLine("stop engine.."); 
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public void travel()
+        {
+            Console.WriteLine("travelling in the car");
+        }
+    }
+
+    class Train : Vehicle
+    {
+        public void travel() 
+        {
+            Console.WriteLine("travelling in the train");
+        }
+    }
+
+    class Aeroplane : Vehicle
+    {
+        public void travel()
+        {
+            Console.WriteLine("travelling in the Plane");
+        }
+    }
+
+}
--- Oops_Concept_Assignment/Program.cs
+++ Oops_Concept_Assignment/Program.cs
@@ -1,9 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+//1) Create a program with object and class concept with example.
 //creating object here
 using Oops_Concept_Assignment;
 
 Class_Object_Concept a=new Class_Object_Concept();
 a.disp();
 
+Console.WriteLine("===================================================");
+
+//2) Create a program with Inheritance concept with example.
+
+//here we are creating child class object which are inheriting properties from parent class
+
+Car car=new Car();
+car.start();
+car.travel();
+car.stop();
+
+        
