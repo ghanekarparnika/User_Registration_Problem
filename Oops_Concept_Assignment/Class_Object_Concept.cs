5da14845c4de14a9bd7432d67ba68d42045d160d
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-05 13:32:58 +0530

    [parnika]:understood class and object functionality

---
+++ Oops_Concept_Assignment/Class_Object_Concept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Concept_Assignment
+{
+    internal class Class_Object_Concept
+    {
+        public void disp()
+        {
+            Console.WriteLine("we are coder....");
+        }
+    }
+}
---
+++ Oops_Concept_Assignment/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//creating object here
+using Oops_Concept_Assignment;
+
+Class_Object_Concept a=new Class_Object_Concept();
+a.disp();
+
