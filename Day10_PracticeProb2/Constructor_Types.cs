514d7a7bb60da1e9a7f8a64d3897deb9fa5d1dd7
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-06 15:17:54 +0530

    [parnika]uderstood all types of constructor

---
+++ Day10_PracticeProb2/Constructor_Types.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_PracticeProb2
+{
+    internal class Constructor_Types
+    {
+        public int Value { get; set; }
+
+        // Default Constructor
+        public Constructor_Types()
+        {
+            Value = 0; // Default initialization
+            Console.WriteLine("Default Constructor");
+        }
+
+        // Parameterized Constructor
+        public Constructor_Types(int value)
+        {
+            Value = value;
+            Console.WriteLine("Parameterized Constructor");
+        }
+
+        // Copy Constructor
+        public Constructor_Types(Constructor_Types other)
+        {
+            Value = other.Value;
+            Console.WriteLine("Copy Constructor");
+        }
+    }
+}
---
+++ Day10_PracticeProb2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Day10_PracticeProb2;
+
+//1) Demonstrate different types of constructors.
+// Default Constructor
+
+
+Console.WriteLine("Q1-Contructor Types");
+Constructor_Types obj1 = new Constructor_Types();
+Console.WriteLine("Value after default constructor: " + obj1.Value);
+
+// Parameterized Constructor
+Constructor_Types obj2 = new Constructor_Types(42);
+Console.WriteLine("Value after parameterized constructor: " + obj2.Value);
+
+// Copy Constructor
+Constructor_Types obj3 = new Constructor_Types(obj2);
+Console.WriteLine("Value after copy constructor: " + obj3.Value);
+
+// Static Constructor (automatically called before any static members are accessed)
+Constructor_Types obj4 = new Constructor_Types();
+
+// Accessing static constructor
+Console.WriteLine("Static constructor has already been called.");
+Console.WriteLine("==============================================");
