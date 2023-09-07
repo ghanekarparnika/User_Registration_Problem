8932b1501d7a85b0b529cfe650544d36e0aac4cd
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-07 14:24:49 +0530

    [parnika]:Given 3 string find the maximum

--- Day13_Practice_Prob/Program.cs
+++ Day13_Practice_Prob/Program.cs
@@ -23,7 +23,7 @@ int result3 = TestMaximum.maxInteger(56, 45, 100);
 Console.WriteLine("max Integer is : " + result3);
 
 Console.WriteLine("=================================================================");
-/*Given 3 Floats find themaximum
+/*UC2-Given 3 Floats find themaximum
 
 - Ensure to test code with the Test Case and*/
 
@@ -37,3 +37,22 @@ Console.WriteLine("max Float is : " + result5);
 //TC 2.3Given Max Number at 3rd Position return the Same Number Ensure the test data has Max Number in the Third position
 float result6 = TestMaximum.maxFloat(20.3f, 10.3f, 70.9f);
 Console.WriteLine("max Float is : " + result6);
+Console.WriteLine("=================================================================");
+
+/*UC3-Given 3 Strings find the maximum
+
+- Ensure to test code with the Test
+Case and repeat with the similar 3
+Test cases in UC 1
+
+UC 3 - E.g. Apple Peach Banana*/
+
+//TC 3.1-Given Max Number at 1st Position return the Same Number Ensure the test data has Max Number in the First position
+string result7 = TestMaximum.maxString("Peach", "Apple", "Banana");
+Console.WriteLine("max lexocographical string is : " + result7);
+//TC 3.2-Given Max Number at 2nd Position return the Same Number Ensure the test data has Max Number in the Second position
+string result8 = TestMaximum.maxString("Apple", "Peach", "Banana");
+Console.WriteLine("max lexocographical string is : " + result8);
+//TC 3.3Given Max Number at 3rd Position return the Same Number Ensure the test data has Max Number in the Third position
+string result9 = TestMaximum.maxString("Apple", "Banana", "Peach");
+Console.WriteLine("max lexocographical string is : " + result9);
--- Day13_Practice_Prob/TestMaximum.cs
+++ Day13_Practice_Prob/TestMaximum.cs
@@ -58,6 +58,31 @@ namespace Day13_Practice_Prob
 
         }
 
+        public static string maxString(string str1, string str2, string str3)
+        {
+            if (str1.CompareTo(str2) > 0 && str2.CompareTo(str2) > 0 ||
+                str1.CompareTo(str2) >= 0 && str1.CompareTo(str3) > 0 ||
+                str1.CompareTo(str2) > 0 && str1.CompareTo(str3) >= 0)
+            {
+                return str1;
+            }
+            if (str2.CompareTo(str1) > 0 && str2.CompareTo(str3) > 0 ||
+                str2.CompareTo(str1) >= 0 && str2.CompareTo(str3) > 0 ||
+                str2.CompareTo(str1) > 0 && str2.CompareTo(str3) >= 0)
+            {
+                return str2;
+            }
+            if (str3.CompareTo(str1) > 0 && str3.CompareTo(str2) > 0 ||
+                str3.CompareTo(str1) >= 0 && str3.CompareTo(str2) > 0 ||
+                str2.CompareTo(str1) > 0 && str3.CompareTo(str2) >= 0)
+            {
+                return str3;
+            }
+
+            throw new Exception("str1,str2,str3 are same");
+        }
+
+
 
     }
 }
