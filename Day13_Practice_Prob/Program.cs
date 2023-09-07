d16bb8b7e71081df7e0b0f4806a9800b7dcd6c55
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-07 14:14:50 +0530

    [parnika]:Given 3 Float find the maximum

--- Day13_Practice_Prob/Program.cs
+++ Day13_Practice_Prob/Program.cs
@@ -11,13 +11,29 @@ UC 1 method to test the maximum number*/
 
 using Day13_Practice_Prob;
 
-//TC1.1-Given Max Number at 1st Position return the Same Number Ensure the test data has Max Number in the First position
+//TC 1.1-Given Max Number at 1st Position return the Same Number Ensure the test data has Max Number in the First position
 int result = TestMaximum.maxInteger(200, 56, 1);
 Console.WriteLine("max Integer is : " + result);
-//TC1.2-Given Max Number at 2nd Position return the Same Number Ensure the test data has Max Number in the Second position
+//TC 1.2-Given Max Number at 2nd Position return the Same Number Ensure the test data has Max Number in the Second position
 int result1 = TestMaximum.maxInteger(56, 200, 1);
 Console.WriteLine("max Integer is : " + result1);
 
-//Given Max Number at 3rd Position return the Same Number Ensure the test data has Max Number in the Third position
+//TC 1.3Given Max Number at 3rd Position return the Same Number Ensure the test data has Max Number in the Third position
 int result3 = TestMaximum.maxInteger(56, 45, 100);
 Console.WriteLine("max Integer is : " + result3);
+
+Console.WriteLine("=================================================================");
+/*Given 3 Floats find themaximum
+
+- Ensure to test code with the Test Case and*/
+
+//TC 2.1-Given Max Number at 1st Position return the Same Number Ensure the test data has Max Number in the First position
+float result4 = TestMaximum.maxFloat(20.3f, 10.3f, 7.9f);
+Console.WriteLine("max Float is : " + result4);
+
+//TC 2.2-Given Max Number at 2nd Position return the Same Number Ensure the test data has Max Number in the Second position
+float result5 = TestMaximum.maxFloat(20.3f, 100.3f, 7.9f);
+Console.WriteLine("max Float is : " + result5);
+//TC 2.3Given Max Number at 3rd Position return the Same Number Ensure the test data has Max Number in the Third position
+float result6 = TestMaximum.maxFloat(20.3f, 10.3f, 70.9f);
+Console.WriteLine("max Float is : " + result6);
--- Day13_Practice_Prob/TestMaximum.cs
+++ Day13_Practice_Prob/TestMaximum.cs
@@ -33,5 +33,31 @@ namespace Day13_Practice_Prob
 
         }
 
+        public static float maxFloat(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num2.CompareTo(num3) > 0 ||
+                num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0 ||
+                num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+            {
+                return num1;
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+                num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+                num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0 ||
+                num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) > 0 ||
+                num2.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+            {
+                return num3;
+            }
+
+            throw new Exception("num1,num2,num3 are same");
+
+        }
+
+
     }
 }
