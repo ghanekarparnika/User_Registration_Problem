838363991bc98f058a4ecf0aa486463786206b3c
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-08 21:26:09 +0530

    [parnika]:Program to print a all unique elements in an array

--- Day11_Assig3/Program.cs
+++ Day11_Assig3/Program.cs
@@ -12,4 +12,9 @@ int[] arr = { 1, 2, 3, 4, 2, 5, 6, 4, 7, 8, 9, 1,2 };
 int count = DuplicateElements.checkDouble_Elememts(arr);
 Console.WriteLine("Total number of duplicate elements: " + count);
 Console.WriteLine("===================================================");
-    
+
+//2) Write a program in C# Sharp to print all unique elements in an array.
+Console.WriteLine("Q2-print count of Unique elements");
+UniqueElements.printUnique();
+Console.WriteLine();
+Console.WriteLine("===================================================");
