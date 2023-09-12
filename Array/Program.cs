9821799a4f7784fb688df874ce4848f8bc239da0
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-12 12:55:51 +0530

    [parnika]:Program to ctrate LinkedList

--- Array/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Array;
-
-//Console.WriteLine("Hello, World!");
-//ArrayDemo.ArrayDeclaration();
-ArrayDemo.ArrayListDeclaration();
---
+++ Linked_List/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//UC1-Lets create a simple Linked List of 56, 30 and 70
+
+using System.Collections.Generic;
+
+LinkedList<int> list = new LinkedList<int>();
+list.AddFirst(56);
+list.AddLast(30);
+list.AddLast(70);
+Console.Write("Created Linked List: ");
+
+foreach (int data in list)
+{
+    Console.Write(data+"-->");
+}
