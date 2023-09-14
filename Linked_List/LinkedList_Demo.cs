c81ce8283c60debbc95bb995b1ac92efef31e8bb
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-14 18:34:05 +0530

    [parnika]:Ability to create linkedlist by appending 30 and 70 to 56

---
+++ Linked_List/LinkedList_Demo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Linked_List
+{
+    internal class LinkedList_Demo
+    {
+        public Node head;
+       /* public void addNode(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine(data + " is inserted into the linkedlist");
+        }*/
+
+
+        public void appendNode(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else 
+            {
+                Node current = head;
+                while (current.Next != null) 
+                {
+                    current = current.Next; ;
+                }
+                current.Next = node;
+            }
+            Console.WriteLine(data + " is appended to the linkedlist");
+        }
+
+        public void Display()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+            }
+            else
+            {
+                Node tempNode = head;
+                while (tempNode != null)
+                {
+                    Console.Write(tempNode.data + "-->");
+                    tempNode = tempNode.Next;
+                }
+            }
+        }
+    }
+}
--- Linked_List/Program.cs
+++ Linked_List/Program.cs
@@ -2,9 +2,10 @@
 //Console.WriteLine("Hello, World!");
 //UC1-Lets create a simple Linked List of 56, 30 and 70
 
+using Linked_List;
 using System.Collections.Generic;
 
-LinkedList<int> list = new LinkedList<int>();
+/*LinkedList<int> list = new LinkedList<int>();
 list.AddFirst(56);
 list.AddLast(30);
 list.AddLast(70);
@@ -13,4 +14,21 @@ Console.Write("Created Linked List: ");
 foreach (int data in list)
 {
     Console.Write(data+"-->");
-}
+}*/
+
+//Ability to create Linked List by adding 30 and 56 to 70
+
+/*
+- Node with data 70 is First Created
+- Next 30 is added to 70
+- Finally 56 is added to 30
+- LinkedList Sequence: 56->30->70*/
+
+LinkedList_Demo link = new LinkedList_Demo();
+//link.addNode(56);
+//link.addNode(70);
+//link.Display();
+link.appendNode(56);
+link.appendNode(30);
+link.appendNode(70);
+link.Display();
