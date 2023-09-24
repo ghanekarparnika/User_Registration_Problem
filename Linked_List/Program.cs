572dbe1b066b6276e66f7fede742c2874e40b2f9
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-25 00:10:38 +0530

    [parnika]:Ability to create ascending order of data entered

--- Linked_List/Program.cs
+++ Linked_List/Program.cs
@@ -1,18 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-//UC1-Lets create a simple Linked List of 56, 30 and 70
 
 using Linked_List;
 using System.Collections.Generic;
 
-/*Ability to delete 40 from the Linked List sequence of 56->30->40->70 and show the size of LinkedList is 3
+//UC10
+SortedLinkedList<int> sortedList = new SortedLinkedList<int>();
+sortedList.Add(56);
+sortedList.Add(30);
+sortedList.Add(40);
+sortedList.Add(70);
 
-- Write MSTest Test Case as demonstrated in class
--Search LinkedList to find node with key value 40
-- Delete the node
-- Implement size() and show the Linked List size is 3
-- Final Sequence: 56->30->70*/
-
-// Main method is used for running tests
-var testClass = new LinkedListTests();
-testClass.TestDeleteAndSize();
+sortedList.Display(); // Output: 30 40 56 70
