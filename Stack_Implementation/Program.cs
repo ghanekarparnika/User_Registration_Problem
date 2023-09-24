bd0bfd02d4dae0b74ea586e05613c1e90e8f72c3
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-25 01:10:54 +0530

    [parnika]:Ability to create a Stack of 56->30->70

---
+++ Stack_Implementation/Program.cs
@@ -0,0 +1,15 @@
+﻿//Console.WriteLine("Hello, World!");
+/*Ability to create a Stack of 56->30->70
+- Use LinkedList to do the Stack Operations
+- Here push will internally call add method on LinkedList.
+- So 70 will be added first then 30 and then 56 to make 56 on top of the Stack*/
+
+using Stack_Implementation;
+
+Stack_Using_LinkedList<int> stack = new Stack_Using_LinkedList<int>();
+stack.Push(70);
+stack.Push(30);
+stack.Push(56);
+
+Console.WriteLine("Top of the stack: " + stack.Peek()); // Output: 56
+
---
+++ Stack_Implementation/Stack_Using_LinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Implementation
+{
+    internal class Stack_Using_LinkedList<T>
+    {
+        private LinkedList<T> linkedList = new LinkedList<T>();
+        public void Push(T data)
+        {
+            linkedList.AddLast(data);
+        }
+        public T Peek()
+        {
+            if (linkedList.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            return linkedList.Last.Value;
+        }
+    }
+}
