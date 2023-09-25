5f4a76ef7cdd5d50ce70bf75488ebcaf547a8ea4
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-25 23:52:41 +0530

    [parnika]:Ability to create a BST by adding 56 and then adding 30 & 70

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using BinarySearchTree;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Tree tree = new Tree();
+        tree.Insert(56);
+        tree.Insert(30);
+        tree.Insert(70);
+
+        Console.WriteLine("preOrder Transversal : Root-left-right");
+        tree.Display(tree.ReturnRoot());
+        Console.WriteLine("Post transversal : left-right-root");
+        tree.Post(tree.ReturnRoot());
+        Console.WriteLine("Inorder transversal : left-root-right");
+        tree.InOrdet(tree.ReturnRoot());
+    }
+
+}
