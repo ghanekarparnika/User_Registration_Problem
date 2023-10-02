69a9c6b71b33a1880c1f7bef385a9b0352313320
ghanekarparnika <parnikaghanekar@gmail.com>
2023-10-02 13:24:06 +0530

    Ability to create a employee payroll table in the payroll service database to manage employee payrolls

--- PayRoll_Service.sql
+++ PayRoll_Service.sql
@@ -5,3 +5,5 @@
 --UC1-Using MSSQL Client use create database query to create a payroll_service database
 Create database PayRoll_Service;
 
+--UC2-Use Create Table employee_payroll Query to create employee payroll table with columns id, name, salary and start date as column.
+create table Employee_Payroll(Id int primary key Identity(1,1),Name varchar(20),Salary float,Start_Date Date);
