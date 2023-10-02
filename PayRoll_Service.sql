2da7aed1b1d99d24c7cbd46eba0b3a554265e2a9
ghanekarparnika <parnikaghanekar@gmail.com>
2023-10-02 13:29:12 +0530

    Ability to create employee payroll data in the payroll service database as part of CRUD Operation

--- PayRoll_Service.sql
+++ PayRoll_Service.sql
@@ -7,3 +7,8 @@ Create database PayRoll_Service;
 
 --UC2-Use Create Table employee_payroll Query to create employee payroll table with columns id, name, salary and start date as column.
 create table Employee_Payroll(Id int primary key Identity(1,1),Name varchar(20),Salary float,Start_Date Date);
+
+--UC3-Insert into Employee_Payroll
+insert into Employee_Payroll values('Raj',60000.0,'2018-06-1');
+insert into Employee_Payroll values('Arya',55000.0,'2020-08-03');
+insert into Employee_Payroll values('Shubham',30000.0,'2023-01-10');
