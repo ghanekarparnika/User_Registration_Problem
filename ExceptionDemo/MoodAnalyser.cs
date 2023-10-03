36a5fe58e5922c4d5384a110a0b36ab331ed8136
ghanekarparnika <parnikaghanekar@gmail.com>
2023-10-03 23:35:01 +0530

    Given “I am in Sad Mood” message Should Return SAD

---
+++ ExceptionDemo/MoodAnalyser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    public class MoodAnalyser
+    {
+
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string analyseMood()
+        {
+            try
+            {
+                if (this.message.Contains("sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (Exception)
+            {
+                return "HAPPY";
+            }
+        }
+    }
+
+}
---
+++ ExceptionDemoTest/UnitTest1.cs
@@ -0,0 +1,35 @@
+using ExceptionDemo;
+
+namespace ExceptionDemoTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSadMoodreturnSAD()
+        {
+            //Arrange
+            string expected = "SAD";
+            string message = "I am in sad mood";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            //Act
+            string actual = moodAnalyser.analyseMood();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GivenNullMoodreturnHAPPY()
+        {
+            //Arrange
+            string expected = "HAPPY";
+            string message = null;
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            //Act
+            string actual = moodAnalyser.analyseMood();
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
