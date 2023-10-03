089bc47acf50875103baeeed39bb1a2a42cff3d9
ghanekarparnika <parnikaghanekar@gmail.com>
2023-10-03 23:50:50 +0530

    Given “I am in Any Mood” message Should Return HAPPY

--- ExceptionDemoTest/UnitTest1.cs
+++ ExceptionDemoTest/UnitTest1.cs
@@ -5,12 +5,26 @@ namespace ExceptionDemoTest
     [TestClass]
     public class UnitTest1
     {
+        //tc1.1
         [TestMethod]
         public void GivenSadMoodreturnSAD()
         {
             //Arrange
             string expected = "SAD";
             string message = "I am in sad mood";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            //Act
+            string actual = moodAnalyser.analyseMood();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        //tc1.2
+        [TestMethod]
+        public void GivenSadMoodreturnHAPPY()
+        {
+            //Arrange
+            string expected = "HAPPY";
+            string message = "I am in any mood";
             MoodAnalyser moodAnalyser = new MoodAnalyser(message);
             //Act
             string actual = moodAnalyser.analyseMood();
