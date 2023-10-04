69e13b0fa52f69dbcbb3ffffad82e1e1a3b8eaff
ghanekarparnika <parnikaghanekar@gmail.com>
2023-10-04 21:46:39 +0530

    First name starts with Cap and has minimum 3 characters

---
+++ User_Registration_Problem/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using User_Registration_Problem;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to User Ragistration  ");
+        Console.WriteLine("---------------------------------------------------");
+        User_Registration.addUserInfo();
+    }
+}
---
+++ User_Registration_Problem/User_Registration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace User_Registration_Problem
+{
+   public class User_Registration
+    {
+
+        public static void addUserInfo()
+        {
+            Console.WriteLine("Enter your first name:");
+            string firstName = Console.ReadLine();
+
+            if (IsValidFirstName(firstName))
+            {
+                Console.WriteLine("Valid first name!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name. It should start with a capital letter and have a minimum of 3 characters.");
+            }
+
+          
+
+        }
+
+
+        //UC1
+        static bool IsValidFirstName(string firstName)
+        {
+            // Define the regular expression pattern for a valid last name.
+            string pattern = @"^[A-Z][A-Za-z]{2,}$";
+
+            // Use Regex.IsMatch to check if the input matches the pattern.
+            return Regex.IsMatch(firstName, pattern);
+        }
+
+
+    }
+}
