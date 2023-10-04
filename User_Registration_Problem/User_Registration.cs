7222b1ea558fd056f9a2c6b4bb5ed4c76c3176a4
ghanekarparnika <parnikaghanekar@gmail.com>
2023-10-04 21:52:14 +0530

    Last name starts with Cap and has minimum 3 characters

--- User_Registration_Problem/User_Registration.cs
+++ User_Registration_Problem/User_Registration.cs
@@ -24,7 +24,19 @@ namespace User_Registration_Problem
                 Console.WriteLine("Invalid first name. It should start with a capital letter and have a minimum of 3 characters.");
             }
 
-          
+            Console.WriteLine("=====================================================");
+
+            Console.WriteLine("Enter your last name:");
+            string lastName = Console.ReadLine();
+
+            if (IsValidLastName(lastName))
+            {
+                Console.WriteLine("Valid last name!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid last name. It should start with a capital letter and have a minimum of 3 characters.");
+            }
 
         }
 
@@ -37,6 +49,16 @@ namespace User_Registration_Problem
 
             // Use Regex.IsMatch to check if the input matches the pattern.
             return Regex.IsMatch(firstName, pattern);
+        }
+
+        //UC2
+        static bool IsValidLastName(string lastName)
+        {
+            // Define the regular expression pattern for a valid last name.
+            string pattern = @"^[A-Z][A-Za-z]{2,}$";
+
+            // Use Regex.IsMatch to check if the input matches the pattern.
+            return Regex.IsMatch(lastName, pattern);
         }
 
 
